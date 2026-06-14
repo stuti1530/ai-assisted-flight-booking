74008c4bf8cc6f1746ee391cd01ce7b416468e68
stuti <stutishende15@gmail.com>
2026-06-14 22:03:20 +0530

    update API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-assisted-flight-booking.onrender.com/",
 });
 
 instance.interceptors.request.use((config) => {
